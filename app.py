2192e461b2f742a7d6fc1fc2a99bf099dde03199
data-detectiv <oppongyaw049@gmail.com>
2025-04-16 19:15:39 -0400

    fastapi and bigquery

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+# Solomons test 
+st.header("Foster and Solomons First Project")
+
+st.write("Hello, world!")
+st.write("Ceated my branch for testing")
+
+
+# Fosters Test 
+# Write your test code below 
+st.write("Hello World! by Foster")
+
+# Second Test
+st.write("Welcome Foster")
